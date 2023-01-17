1d96832356b47ecf52f6a80a362aeb8a8f521d0b
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2023-01-17 14:00:22 -0300

    feat: creates sign in with signed jwt token feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,21 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dtos';
 import { User } from './user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   public async createUser({
@@ -33,5 +37,18 @@ export class AuthService {
       }
       throw new InternalServerErrorException();
     }
+  }
+
+  public async signIn({
+    username,
+    password,
+  }: AuthCredentialsDto): Promise<{ accessToken: string }> {
+    const user = await this.usersRepository.findOneBy({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
+    }
+    throw new UnauthorizedException('Please chech your login credentials');
   }
 }
