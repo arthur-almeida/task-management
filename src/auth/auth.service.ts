6256a5e08ce63173bb8c77f92df92987fcdbbeac
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2023-01-17 10:57:48 -0300

    feat: adds error handling when creating a user with already existing username

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
@@ -15,6 +19,13 @@ export class AuthService {
       username,
       password,
     });
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
