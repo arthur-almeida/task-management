18575e3933fa755bae3fa50807d21c36a1a481b5
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2023-01-17 10:39:06 -0300

    feat: creates create user feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -8,4 +9,12 @@ export class AuthService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
+
+  async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
+    const user = this.usersRepository.create({
+      username,
+      password,
+    });
+    await this.usersRepository.save(user);
+  }
 }
